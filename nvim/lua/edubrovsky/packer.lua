58f1feba4e7adbf16dc23fa11ef3cd558ea6ecaa
Eduard Dubrovsky <edubrovskii@evolution.com>
2023-08-05 18:43:40 +0200

    NVim small fix

--- nvim/lua/edubrovsky/packer.lua
+++ nvim/lua/edubrovsky/packer.lua
@@ -9,7 +9,6 @@ return require('packer').startup(function(use)
 
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plen:ary.nvim'} }
+	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 end)
