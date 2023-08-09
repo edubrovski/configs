06b61c14974d55e6ae6e1207467359cf64a7d958
Eduard Dubrovsky <edubrovskii@evolution.com>
2023-08-09 16:10:31 +0200

    Nvim: add 'code actions' mapping for LSP

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -43,6 +43,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<M-CR>", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
